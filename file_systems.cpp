6ef48cf775a419e4704a6968f27414a84b4cbc62
Nixon Lazaro <44237350+nlazaro@users.noreply.github.com>
2021-12-21 01:50:22 -0500

    !

--- file_systems.cpp
+++ file_systems.cpp
@@ -4,37 +4,57 @@
 #include <string> 
 #include <vector>
 #include <map>
-#include <stdlib.h> //random
+#include <stdlib.h>
+#include <cctype>
 
 //#include <stdlib.h>
 //#include <utility>
 
 using namespace std;
 int main(){
-    
-    ofstream ofs("drive.txt");
-    ofs << "test test" << endl;
-    ofs.close();
-    
-    /*
-    cout << "Type 'exit' to close the terminal." << endl;
-    cout << "Type 'help' for help." << endl;
-
     string input;
+    // Checks for allocation type
+    while(ALLOCATION_TYPE_ACCEPT != 1){
+        cout << "What type of allocation type would you like to use?\n Type 'Linked List' or 'Index list': " << endl;
+        getline(cin, input);
+        ALLOCATION_TYPE = makeLowerCase(input);
+        if(ALLOCATION_TYPE == "linked list"){
+            cout << "You have chosen linked list, and your disk will be called 'drive1.txt'.\n";
+            ALLOCATION_TYPE_ACCEPT = 1;
+                }
+        else if(ALLOCATION_TYPE == "index list"){
+            cout << "You have chosen index list and your disk will be called 'drive2.txt'.\n";
+            ALLOCATION_TYPE_ACCEPT = 1;
+                }
+        else{
+            cout << "Invalid entry, try again.\n";
+            ALLOCATION_TYPE = "";
+                }
+            }
+    cout << "'root' is your root directory." << endl;
+    // The main area of the terminal
     while(true){
-        cin >> input;
-        if(input == "exit"){
-            break;
-        }
-        else if(input == "help"){
-            help();
-        }
+        cout << "Type 'exit' to close the terminal." << endl;
+        cout << "Type 'help' for help." << endl;
+        input = "";
+        getline(cin, input);
+        input = makeLowerCase(input);
+        if(input == "exit") return 0;
+        else if(input == "help") help();
+        /*else if(input == "ls")
+        else if(input == "cd")
+        else if(input == "du")
+        else if(input == "mkdir")
+        else if(input == "rmdir")
+        else if(input == "create")
+        else if(input == "write")
+        else if(input == "read")
+        else if(input == "delete")
+        */
     }
-
-    */
-   //cout << "Enter the name of your file." << endl;
-  // getline(cin, FILE_NAME);
-  createLinkedList();
+   cout << "Enter the name of your file." << endl;
+   /*getline(cin, FILE_NAME);
+   createLinkedList();
    cout << "Enter the content of your file: " << endl;
    getline(cin, FILE_CONTENT);
    TEMP_DATA = convertFileContentIntoBlocks(FILE_CONTENT);
@@ -42,5 +62,6 @@ int main(){
        cout<<TEMP_DATA[i] << endl;
    }
    randomlyAssignFileContent(TEMP_DATA);
+   */
    return 0;
 }
--- file_systems.h
+++ file_systems.h
@@ -13,68 +13,47 @@ using namespace std;
 
 const int BLOCK_LIMIT = 8;
 const int MAX_MEMORY = 5120;
+bool ALLOCATION_TYPE_ACCEPT = 0;
+string input;
+string ALLOCATION_TYPE;
 string FILE_NAME;
 string FILE_CONTENT;
 vector<string> TEMP_DATA = {};
+map<string,int> INODE;
 map<int,int> FAT;
 
 // Creates the node class for linked lists
 class Node{
-    public:
+public:
     int BLOCK_NUMBER;
     string DATA;
     bool FREE_NODE = 0;
     Node* NEXT_PTR;
 };
 
-// Displays the help commands on the terminal
-void help(){
-    cout << "Avaliable commands: \n"
-        << "pwd                         Displays full path of current directory.\n"
-        << "ls                          Displays all filesin current directory.\n"
-        << "cd                          Moves current directory.\n"
-        << "du                          Displays memory of specified file.\n"
-        << "mkdir                       Creates directory.\n"
-        << "rmdir                       Deletes directory.\n"
-        << "create                      Creates file.\n"
-        << "write                       Writes inside of the file.\n"
-        << "read                        Reads inside of the file.\n"
-        << "help                        Shows this command list.\n"
-        << "exit                        Exits this terminal screen.\n";
-}
-
-// Converts user input for file content into 8 byte blocks
-vector<string> convertFileContentIntoBlocks(string file_content){
-    string TEMP_HOLDER = "";
-    vector<string> data = {};
-    int position = 0;
-    int block_limit_copy = BLOCK_LIMIT - 1;
-    // loops through file content
-    for(position; position < file_content.size(); position++){
-        TEMP_HOLDER += file_content[position];
-        // adds into blocks using vector
-        if(block_limit_copy == position){
-            data.push_back(TEMP_HOLDER);
-            TEMP_HOLDER = "";
-            block_limit_copy += 7;
-            }
-        }
-        // adds remiaining contents
-        data.push_back(TEMP_HOLDER);
-        return data;
-    }
+class LinkedList{
+private:
+    Node* head_ptr;
+    Node* last_ptr;
+    Node* temp_ptr;
+public:
+    LinkedList();
+    vector<string> convertFileContentIntoBlocks(string file_contents);
+    void updateInode(string fileName, int startingAddress);
+    void updateFAT(int start_address, int pointer_towards_next_block);
+    int checkNode(Node *n, int key);
+    int RandomBlock(int data);
+    void randomlyAssignFileContent(string fileName, vector<string> data);
+    ~LinkedList();
+    
+};
 
 // Creates a linked list limit of 5,120 blocks
-void createLinkedList(){
-    // Creates head node
-    Node* head_ptr = new Node;
-    Node* temp_ptr;
-    Node* last_ptr;
-
-    head_ptr->BLOCK_NUMBER=0;
+LinkedList::LinkedList(){
+    head_ptr = new Node;
+    head_ptr->BLOCK_NUMBER = 0;
     head_ptr->NEXT_PTR = nullptr;
     last_ptr = head_ptr;
-
     //Creates the remaining 5,119 nodes
     for(int i = 1; i < MAX_MEMORY - 1; i++){
         // Creates new node
@@ -98,28 +77,118 @@ void createLinkedList(){
     */
 }
 
-// Randomly Assigns blocks towards file content and saves on FAT table
-void randomlyAssignFileContent(vector<string> data){
-    int assigned_block_number;
-    int following_block_number;
-    for(int i = 0; i < data.size(); i++){
-        assigned_block_number = rand() % 5120; //excludes the final node on purpose
-        following_block_number = rand() % 5120;
-        updateFAT
+LinkedList::~LinkedList(){
+    Node* p = head_ptr;
+    while(head_ptr){
+        head_ptr = head_ptr->NEXT_PTR;
+        delete p;
+        p = head_ptr;
     }
-    
+}
+
+// Converts user input for file content into 8 byte blocks
+vector<string> LinkedList::convertFileContentIntoBlocks(string file_content){
+    string TEMP_HOLDER = "";
+    vector<string> data = {};
+    int position = 0;
+    int block_limit_copy = BLOCK_LIMIT - 1;
+    // loops through file content
+    for(position; position < file_content.size(); position++){
+        TEMP_HOLDER += file_content[position];
+        // adds into blocks using vector
+        if(block_limit_copy == position){
+            data.push_back(TEMP_HOLDER);
+            TEMP_HOLDER = "";
+            block_limit_copy += 7;
+            }
+        }
+        // adds remiaining contents
+        data.push_back(TEMP_HOLDER);
+        return data;
+    }
+
+void LinkedList::updateInode(string fileName, int startingAddress){
+    INODE.insert(pair<string,int>(fileName, startingAddress));
 }
 
 // Updates FAT table
-void updateFAT(int start_address, int pointer_towards_next_block){
+void LinkedList::updateFAT(int start_address, int pointer_towards_next_block){
     FAT.insert(pair<int,int>(start_address, pointer_towards_next_block));
 }
 
-// Checks FAT for unused blocks
-bool checkFAT(int address){
-
+// Checks nodes for unused blocks
+int LinkedList::checkNode(Node *n, int key){
+    while(n!=NULL){
+        if(key==n->BLOCK_NUMBER){
+          n->FREE_NODE;
+          if(0==n->FREE_NODE){ //node is free to use
+            n->FREE_NODE=1; //change to used
+            return 1; //success
+        }
+        else{
+          return 0; //node is in use;
+          }
+        }
+        n=n->NEXT_PTR;
+    }
+    return 0;
 }
-    /* maps
+
+// Assign random avalible block unit
+int LinkedList::RandomBlock(int random_data){
+    random_data = rand() % 5120;
+    if(checkNode(head_ptr, random_data) == 0){
+       RandomBlock(random_data);
+    }
+    return random_data;
+}
+
+// Randomly Assigns blocks towards file content and saves on FAT table
+void LinkedList::randomlyAssignFileContent(string fileName, vector<string> data){
+    int starting_block_number;
+    int following_block_number;
+    int temp;
+    starting_block_number = RandomBlock(0);
+    updateInode(fileName, starting_block_number);
+    for(int i=1; i < data.size(); i++){
+        starting_block_number = RandomBlock(i);
+        if(data.size() > 0){
+            following_block_number = RandomBlock(i+1);
+            i++;
+            updateFAT(starting_block_number, following_block_number);
+            }
+        }
+    following_block_number = -1;
+    updateFAT(starting_block_number, following_block_number);
+}
+
+/////////////////////
+// returns input in lower case
+string makeLowerCase(string word){
+    string temp = "";
+    for(int i = 0; i < word.length(); i++){
+        temp += tolower(word[i]);
+        }
+    return temp;
+}
+
+// Displays the help commands on the terminal
+void help(){
+    cout << "Avaliable commands: \n"
+        << "pwd                         Displays full path of current directory.\n"
+        << "ls                          Displays all files in current directory.\n"
+        << "cd                          Moves current directory.\n"
+        << "du                          Displays memory of specified file.\n"
+        << "mkdir                       Creates directory.\n"
+        << "rmdir                       Deletes directory.\n"
+        << "create                      Creates file.\n"
+        << "write                       Writes inside of the file.\n"
+        << "read                        Reads inside of the file.\n"
+        << "help                        Shows this command list.\n"
+        << "exit                        Exits this terminal screen.\n";
+}
+
+/* maps
     map<int,int> fat;
     fat.insert(pair<int,int>(1,3));
     fat.insert(pair<int,int>(3,2));
